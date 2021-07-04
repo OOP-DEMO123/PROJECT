a9d66742a7de6e01fd55e74a68184b2b844d87c9
vuong <vuong@DESKTOP-B3AS2R3>
2021-07-04 12:44:16 +0700

    vuongnm-update

--- src/com/poly/dao/UsersDAO.java
+++ src/com/poly/dao/UsersDAO.java
@@ -6,13 +6,16 @@
 package com.poly.dao;
 
 import com.poly.entity.users;
+import com.poly.utils.XJdbc;
+import java.util.ArrayList;
 import java.util.List;
+import java.sql.ResultSet;
 
 /**
  *
  * @author vuong
  */
-public class UsersDAO extends DADAO<users,String>{
+public class UsersDAO extends DADAO<users, String> {
 
     @Override
     public void insert(users entity) {
@@ -36,14 +39,32 @@ public class UsersDAO extends DADAO<users,String>{
 
     @Override
     public List<users> selectAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "SELECT * FROM users";
+        return selectBySql(sql);
     }
 
     @Override
     protected List<users> selectBySql(String sql, Object... args) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<users> list = new ArrayList<>();
+        try {
+            ResultSet rs = null;
+            try {
+                rs = XJdbc.query(sql, args);
+                while (rs.next()) {
+                    users entity = new users();
+                    entity.setUsername(rs.getString("username"));
+                    entity.setPassword(rs.getString("password"));
+                    entity.setImage(rs.getString("image"));
+                    entity.setEmail(rs.getString("email"));
+                    list.add(entity);
+                }
+            } finally {
+                rs.getStatement().getConnection().close();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return list;
     }
 
-   
-    
 }
